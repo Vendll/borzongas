e7fee3f62724f84faadceb1c916deb0c4b55ce15
Vendll <utto.vendel@icloud.com>
2022-09-04 18:04:51 +0200

    crossOrigin

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html className="bg-white antialiased" lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,400;0,700;1,400&family=Montserrat:ital,wght@0,400;0,700;1,400&display=swap"
           rel="stylesheet"
