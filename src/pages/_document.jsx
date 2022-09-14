7d54e60f75086b0104c47cff05b61db29f54a5b1
Vendll <utto.vendel@icloud.com>
2022-09-14 15:34:03 +0200

    ga

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -7,23 +7,6 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,400;0,700;1,400&family=Montserrat:ital,wght@0,400;0,700;1,400&display=swap"
-          rel="stylesheet"
-        />
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-0MWZ3RQ41R"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-0MWZ3RQ41R');
-        `}
-        </Script>
       </Head>
       <body>
         <Main />
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,6 +7,7 @@ import { Newsletter } from '@/components/Newsletter'
 import { Schedule } from '@/components/Schedule'
 import { Speakers } from '@/components/Speakers'
 import { Sponsors } from '@/components/Sponsors'
+import Script from 'next/script'
 
 export default function Home() {
   return (
@@ -15,6 +16,23 @@ export default function Home() {
         <title>Soproni Borzsongás 2022</title>
         <meta name="description" content="Soproni Borzsongás 2022" />
       </Head>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,400;0,700;1,400&family=Montserrat:ital,wght@0,400;0,700;1,400&display=swap"
+        rel="stylesheet"
+      />
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-0MWZ3RQ41R"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-0MWZ3RQ41R');
+        `}
+      </Script>
       <Header />
       <main>
         <Hero />
