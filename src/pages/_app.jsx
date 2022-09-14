56e90d093ee43843f5a64ea7671e4cbf3adfbc6b
Vendll <utto.vendel@icloud.com>
2022-09-14 15:42:26 +0200

    ga

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,23 @@
 import 'focus-visible'
 import '@/styles/tailwind.css'
-
+import Script from 'next/script'
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-0MWZ3RQ41R"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-0MWZ3RQ41R');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,19 +20,7 @@ export default function Home() {
         href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,400;0,700;1,400&family=Montserrat:ital,wght@0,400;0,700;1,400&display=swap"
         rel="stylesheet"
       />
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-0MWZ3RQ41R"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
 
-          gtag('config', 'G-0MWZ3RQ41R');
-        `}
-      </Script>
       <Header />
       <main>
         <Hero />
